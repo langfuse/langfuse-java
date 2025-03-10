e071986c93cd16ffd9f9aa98d8f1f350d4ff557f
Steffen Schmitz <steffen@langfuse.com>
2025-03-10 15:10:22 +0100

    chore: update api endpoints and add CI (#4)

--- src/main/java/com/langfuse/client/resources/trace/TraceClient.java
+++ src/main/java/com/langfuse/client/resources/trace/TraceClient.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.langfuse.client.core.ClientOptions;
 import com.langfuse.client.core.LangfuseClientApiException;
 import com.langfuse.client.core.LangfuseClientException;
+import com.langfuse.client.core.MediaTypes;
 import com.langfuse.client.core.ObjectMappers;
 import com.langfuse.client.core.QueryStringMapper;
 import com.langfuse.client.core.RequestOptions;
@@ -18,6 +19,7 @@ import okhttp3.Headers;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 import com.langfuse.client.resources.commons.errors.AccessDeniedError;
@@ -26,7 +28,9 @@ import com.langfuse.client.resources.commons.errors.MethodNotAllowedError;
 import com.langfuse.client.resources.commons.errors.NotFoundError;
 import com.langfuse.client.resources.commons.errors.UnauthorizedError;
 import com.langfuse.client.resources.commons.types.TraceWithFullDetails;
+import com.langfuse.client.resources.trace.requests.DeleteTracesRequest;
 import com.langfuse.client.resources.trace.requests.GetTracesRequest;
+import com.langfuse.client.resources.trace.types.DeleteTraceResponse;
 import com.langfuse.client.resources.trace.types.Traces;
 
 public class TraceClient {
@@ -67,6 +71,58 @@ public class TraceClient {
       ResponseBody responseBody = response.body();
       if (response.isSuccessful()) {
         return ObjectMappers.JSON_MAPPER.readValue(responseBody.string(), TraceWithFullDetails.class);
+      }
+      String responseBodyString = responseBody != null ? responseBody.string() : "{}";
+      try {
+        switch (response.code()) {
+          case 400:throw new Error(ObjectMappers.JSON_MAPPER.readValue(responseBodyString, Object.class));
+          case 401:throw new UnauthorizedError(ObjectMappers.JSON_MAPPER.readValue(responseBodyString, Object.class));
+          case 403:throw new AccessDeniedError(ObjectMappers.JSON_MAPPER.readValue(responseBodyString, Object.class));
+          case 404:throw new NotFoundError(ObjectMappers.JSON_MAPPER.readValue(responseBodyString, Object.class));
+          case 405:throw new MethodNotAllowedError(ObjectMappers.JSON_MAPPER.readValue(responseBodyString, Object.class));
+        }
+      }
+      catch (JsonProcessingException ignored) {
+        // unable to map error response, throwing generic error
+      }
+      throw new LangfuseClientApiException("Error with status code " + response.code(), response.code(), ObjectMappers.JSON_MAPPER.readValue(responseBodyString, Object.class));
+    }
+    catch (IOException e) {
+      throw new LangfuseClientException("Network error executing HTTP request", e);
+    }
+  }
+
+  /**
+   * Delete a specific trace
+   */
+  public DeleteTraceResponse delete(String traceId) {
+    return delete(traceId,null);
+  }
+
+  /**
+   * Delete a specific trace
+   */
+  public DeleteTraceResponse delete(String traceId, RequestOptions requestOptions) {
+    HttpUrl httpUrl = HttpUrl.parse(this.clientOptions.environment().getUrl()).newBuilder()
+      .addPathSegments("api/public")
+      .addPathSegments("traces")
+      .addPathSegment(traceId)
+      .build();
+    Request okhttpRequest = new Request.Builder()
+      .url(httpUrl)
+      .method("DELETE", null)
+      .headers(Headers.of(clientOptions.headers(requestOptions)))
+      .addHeader("Content-Type", "application/json")
+      .addHeader("Accept", "application/json")
+      .build();
+    OkHttpClient client = clientOptions.httpClient();
+    if (requestOptions != null && requestOptions.getTimeout().isPresent()) {
+      client = clientOptions.httpClientWithTimeout(requestOptions);
+    }
+    try (Response response = client.newCall(okhttpRequest).execute()) {
+      ResponseBody responseBody = response.body();
+      if (response.isSuccessful()) {
+        return ObjectMappers.JSON_MAPPER.readValue(responseBody.string(), DeleteTraceResponse.class);
       }
       String responseBodyString = responseBody != null ? responseBody.string() : "{}";
       try {
@@ -159,6 +215,65 @@ public class TraceClient {
         ResponseBody responseBody = response.body();
         if (response.isSuccessful()) {
           return ObjectMappers.JSON_MAPPER.readValue(responseBody.string(), Traces.class);
+        }
+        String responseBodyString = responseBody != null ? responseBody.string() : "{}";
+        try {
+          switch (response.code()) {
+            case 400:throw new Error(ObjectMappers.JSON_MAPPER.readValue(responseBodyString, Object.class));
+            case 401:throw new UnauthorizedError(ObjectMappers.JSON_MAPPER.readValue(responseBodyString, Object.class));
+            case 403:throw new AccessDeniedError(ObjectMappers.JSON_MAPPER.readValue(responseBodyString, Object.class));
+            case 404:throw new NotFoundError(ObjectMappers.JSON_MAPPER.readValue(responseBodyString, Object.class));
+            case 405:throw new MethodNotAllowedError(ObjectMappers.JSON_MAPPER.readValue(responseBodyString, Object.class));
+          }
+        }
+        catch (JsonProcessingException ignored) {
+          // unable to map error response, throwing generic error
+        }
+        throw new LangfuseClientApiException("Error with status code " + response.code(), response.code(), ObjectMappers.JSON_MAPPER.readValue(responseBodyString, Object.class));
+      }
+      catch (IOException e) {
+        throw new LangfuseClientException("Network error executing HTTP request", e);
+      }
+    }
+
+    /**
+     * Delete multiple traces
+     */
+    public DeleteTraceResponse deleteMultiple(DeleteTracesRequest request) {
+      return deleteMultiple(request,null);
+    }
+
+    /**
+     * Delete multiple traces
+     */
+    public DeleteTraceResponse deleteMultiple(DeleteTracesRequest request,
+        RequestOptions requestOptions) {
+      HttpUrl httpUrl = HttpUrl.parse(this.clientOptions.environment().getUrl()).newBuilder()
+        .addPathSegments("api/public")
+        .addPathSegments("traces")
+        .build();
+      RequestBody body;
+      try {
+        body = RequestBody.create(ObjectMappers.JSON_MAPPER.writeValueAsBytes(request), MediaTypes.APPLICATION_JSON);
+      }
+      catch(JsonProcessingException e) {
+        throw new LangfuseClientException("Failed to serialize request", e);
+      }
+      Request okhttpRequest = new Request.Builder()
+        .url(httpUrl)
+        .method("DELETE", body)
+        .headers(Headers.of(clientOptions.headers(requestOptions)))
+        .addHeader("Content-Type", "application/json")
+        .addHeader("Accept", "application/json")
+        .build();
+      OkHttpClient client = clientOptions.httpClient();
+      if (requestOptions != null && requestOptions.getTimeout().isPresent()) {
+        client = clientOptions.httpClientWithTimeout(requestOptions);
+      }
+      try (Response response = client.newCall(okhttpRequest).execute()) {
+        ResponseBody responseBody = response.body();
+        if (response.isSuccessful()) {
+          return ObjectMappers.JSON_MAPPER.readValue(responseBody.string(), DeleteTraceResponse.class);
         }
         String responseBodyString = responseBody != null ? responseBody.string() : "{}";
         try {
